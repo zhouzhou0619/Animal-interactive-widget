3d9557cec5dfb9e962cd08255a53002aca83dd59
zhouzhou <zhouzzhou0619@gmail.com>
2023-04-05 21:07:45 +1000

    9:04PM_5/04/2023

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import AnimalShow from './AnimalShow';
 
 function getRandomAnimal() {
     const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
@@ -20,12 +21,15 @@ function App() {
         setAnimals([...animals, getRandomAnimal()]);
     };
     
+    const renderedAnimals = animals.map((animal, index) => {
+        return <AnimalShow type={animal} key={index} />;
+    });
 
 
     return (
     <div>
         <button onClick={handleClick}>Add Animal</button>
-        <div>{animals}</div>
+        <div>{renderedAnimals}</div>
     </div>
     );
 }
