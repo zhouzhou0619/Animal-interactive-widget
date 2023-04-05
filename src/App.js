a9872eaed9b8008e2bf12fb5da7b1af23c99e388
zhouzhou <zhouzzhou0619@gmail.com>
2023-04-05 14:55:21 +1000

    2:55PM_5/04/2023

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import { useState } from 'react';
 
-function App() {
-    const [count, setCount] = useState(0);
-    const handleClick = () => {
-        setCount(count + 1);
+function getRandomAnimal() {
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
 
+    return animals[Math.floor(Math.random() * animals.length)];
+
+
+}
+
+
+
+function App() {
+    // const [count, setCount] = useState(0);
+    const [animals, setAnimals] = useState([]);
+
+    const handleClick = () => {
+        // setCount(count + 1);
+        // animals.push(getRandomAnimal())
+        setAnimals([...animals, getRandomAnimal()]);
     };
     
 
@@ -12,7 +25,7 @@ function App() {
     return (
     <div>
         <button onClick={handleClick}>Add Animal</button>
-        <div>Number of animals: {count}</div>
+        <div>{animals}</div>
     </div>
     );
 }
